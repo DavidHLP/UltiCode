d61f45cbe7557d0e3a4e7980c9cd615a3000f8b6
DavidHLP <lysf15520112973@163.com>
2026-05-12 14:55:39 +0800

    chore: add stdio inherit to maven start scripts and bump vue-jsx plugin

--- backend-spring/start.cjs
+++ backend-spring/start.cjs
@@ -6,6 +6,7 @@ const mvnw = resolve(__dirname, 'mvnw');
 
 const proc = spawn(mvnw, ['spring-boot:run', '-Dmaven.test.skip=true', `-Dspring-boot.run.jvmArguments=-XX:-UseContainerSupport`], {
   cwd: __dirname,
+  stdio: 'inherit',
 });
 
 proc.on('close', (code) => process.exit(code));

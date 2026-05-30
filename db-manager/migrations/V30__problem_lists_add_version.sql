4b0f1e898b127fe44c12580654a18f679d31a6ce
DavidHLP <lysf15520112973@163.com>
2026-05-30 13:19:32 +0800

    chore: Remove db-manager directory

--- db-manager/migrations/V30__problem_lists_add_version.sql
+++
@@ -1,8 +0,0 @@
--- Add version column for optimistic locking on problem_lists table
-SET FOREIGN_KEY_CHECKS=0;
-
-ALTER TABLE `problem_lists` ADD COLUMN `version` INT NOT NULL DEFAULT 1 AFTER `banner_order`;
-
-CREATE INDEX `idx_version` ON `problem_lists` (`version`);
-
-SET FOREIGN_KEY_CHECKS=1;

c249c5694703b4cd1c28d0bbcd4abb099afda90e
DavidHLP <lysf15520112973@163.com>
2026-06-03 22:04:45 +0800

    fix: constrain management views and theme scrollbars

--- management/src/views/notifications/NotificationsListView.vue
+++ management/src/views/notifications/NotificationsListView.vue
@@ -265,7 +265,7 @@ const columns: ColumnDef<SystemAnnouncement>[] = [
 </script>
 
 <template>
-  <div class="relative flex flex-col gap-0 overflow-auto">
+  <div class="relative flex flex-col gap-0 w-full min-w-0">
     <!-- Terminal Header -->
     <div
       :class="[
